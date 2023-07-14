bcda821baaa9910bf79739da020fa18b3a0f7a96
DuckWichTrust <68964814+AmirhosseinAghighi@users.noreply.github.com>
2023-07-14 02:13:28 +0200

    Update config.lua

--- client/config.lua
+++ client/config.lua
@@ -1,7 +1,7 @@
 Config = Config or {}
 
 Config.Peds = {
-    [GetHashKey('a_c_mtlion')] = {time = 10000, name = 'a_c_mtlion'} ,
-    [GetHashKey('a_c_deer')]  = {time = 8000, name = 'a_c_deer'} ,
+    [GetHashKey('a_c_mtlion')] = {name = 'a_c_mtlion'} ,
+    [GetHashKey('a_c_deer')]  = {name = 'a_c_deer'} ,
 }
 
